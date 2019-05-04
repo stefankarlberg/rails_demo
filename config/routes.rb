12f2c130ded81458eed3c7629662007e2f527022
stefan_karlberg <stefan.karlberg@smackit.se>
2019-05-04 22:20:57 +0200

    Adds view and controller to new article, still not correct routed. But the rails puzzle feels little less fuzzy right now. Awesome.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  root controller: :articles, action: :index
+  
+  get root_path controller: :articles, action: :index
+
+  get 'articles/new'
+  root controller: :articles, action: :new
+
 end
